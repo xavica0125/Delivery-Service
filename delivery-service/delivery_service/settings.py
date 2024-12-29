ebd9b6032b98fa9a11700cb098758b70d7b31b82
xavica0125 <123427635+xavica0125@users.noreply.github.com>
2024-12-29 22:38:31 +0000

    FEAT: Add Celery Beat scheduler and task to fetch fuel prices from EIA API

--- delivery-service/delivery_service/settings.py
+++ delivery-service/delivery_service/settings.py
@@ -153,3 +153,6 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = "django-db"
+# CELERY BEAT SCHEDULER
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
--- delivery-service/users/task.py
+++ delivery-service/users/task.py
@@ -6,6 +6,11 @@ from celery import shared_task
 from django.conf import settings
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
+import requests
+from django.utils import timezone
+from .models import FuelPrice
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
+from decimal import Decimal
 
 
 @shared_task
@@ -40,3 +45,31 @@ def send_password_reset_email(user_id, domain, protocol, token):
 
         # Send the email
         server.sendmail(settings.EMAIL_USER, user.email, msg.as_string())
+
+
+@shared_task
+def get_fuel_prices():
+    most_recent_price_date = str(timezone.localtime(timezone.now()))[:10]
+    r = requests.get(
+        f"https://api.eia.gov/v2/petroleum/pri/gnd/data?api_key={settings.EIA_KEY}&frequency=weekly&data[0]=value&facets[duoarea][]=NUS&facets[duoarea][]=STX&facets[product][]=EPD2DXL0&facets[product][]=EPMPU&facets[series][]=EMD_EPD2DXL0_PTE_NUS_DPG&facets[series][]=EMM_EPMPU_PTE_STX_DPG&facets[process][]=PTE&start={most_recent_price_date}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+    )
+
+    response_dict = r.json()
+    price_list = [
+        response_dict["response"]["data"][x]["value"] for x in range(2)
+    ]  # From response_dict, add price values to list, these are strings
+
+    FuelPrice.objects.create(
+        gas_price=Decimal(price_list[0]), diesel_price=Decimal(price_list[1])
+    )
+
+
+"""schedule, _ = CrontabSchedule.objects.get_or_create(
+    minute="0", hour="7", day_of_week="monday"
+)
+
+task, _ = PeriodicTask.objects.get_or_create(
+    crontab=schedule,
+    name="Get fuel prices",
+    task="users.task.get_fuel_prices",
+)"""
