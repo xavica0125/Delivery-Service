66955ec895f1107430d59346f9ad61bd8b5c6fb5
Xavier Cabrera Alvarez <123427635+xavica0125@users.noreply.github.com>
2025-06-19 03:53:44 +0000

    FEAT: Updated marker glyph icons

--- users/static/js/map.js
+++ users/static/js/map.js
@@ -38,8 +38,8 @@ function initMap() {
       provideRouteAlternatives: true,
     })
     .then((response) => {
-      createMarker(originCoords, "green");
-      createMarker(destinationCoords, "red");
+      createMarker(originCoords, "green", "origin");
+      createMarker(destinationCoords, "red", "destination");
       directionsRenderer.setDirections(response);
       directionsRenderer.setRouteIndex(1);
       
@@ -51,12 +51,28 @@ function initMap() {
 
 // Make custom marker elements to display alongside route
 
-function createMarker(coords, color) {
+function createMarker(coords, color, markerType) {
+  const parser = new DOMParser();
+  let pinSvgString;
+  if(markerType === "origin")
+  {
+    pinSvgString = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-house-fill' viewBox='0 0 16 16'> <path d='M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z'/> <path d='m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z'/> </svg>";
+  }
+  else
+  {
+    pinSvgString = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-flag-fill' viewBox='0 0 16 16'> <path d='M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001'/></svg>"
+  }
+  const pinSvg = parser.parseFromString(
+    pinSvgString,
+    "image/svg+xml",
+  ).documentElement;
+  
   const pin = new google.maps.marker.PinElement({
     scale : 1,
     background : color,
     borderColor : color,
-    glyphColor: "white",
+    glyph: pinSvg,
+    glyphColor : "white"
   });
 
   const marker = new google.maps.marker.AdvancedMarkerElement({
